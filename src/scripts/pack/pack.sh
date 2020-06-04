b7e7d6d899271c3629350c05fff344c5dd72f89f
MGlauer <martinglauer89@gmail.com>
2020-06-04 13:38:55 +0200

    Adapt pack script to new IRI schema

--- src/scripts/pack/pack.sh
+++ src/scripts/pack/pack.sh
@@ -2,6 +2,8 @@
 
 out=$1
 
+OEO_VERSION="1.0.0"
+
 if [[ -z $out ]]; then
     out="out"
 fi
@@ -9,33 +11,37 @@ fi
 function convert {
 
     filepath=$1
+    ontology=$2
+    version=$3
+    target_extension=$4
+
+    if [[ $version == OEO_VERSION ]] ; then
+        version="$OEO_VERSION"
+    fi
+
     fullfilename=$(basename -- "$filepath")
     extension="${fullfilename##*.}"
-    target_extension=$2
     filename="${fullfilename%.*}"
-    mkdir -p $out/$filename 
+    mkdir -p $out/$ontology/$version
     if [[ ! $extension == $target_extension ]]; then
-        robot convert --input $filepath --output $out/$filename/$filename.$target_extension --format $target_extension
+        robot convert --input $filepath --output $out/$ontology/$version/$filename.$target_extension --format $target_extension
     else
-        cp $filepath $out/$filename/$filename.$extension
+        cp $filepath $out/$ontology/${version}/$filename.$extension
     fi
 }
 
-function convert_all {
-
-    echo $1 $2
-
-}
 
 cat pack.conf | while read -r line ; do
     linearr=($line)
     path=${linearr[0]}
-    formats=${linearr[1]}
+    ontology=${linearr[1]}
+    version=${linearr[2]}
+    formats=${linearr[3]}
     if [[ ! $path == \#* ]] && [ ! -z "$line" ]; then
         IFS=','
         read -a formatarr <<< "$formats"
         for format in ${formats}; do
-            convert ../../../$path $format
+            convert ../../../$path $ontology $version $format
         done
         IFS=' '
     fi
