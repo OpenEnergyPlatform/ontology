cef8d1d798e8fa5c139010a8fd153c45c287ac40
stage1407 <86624154+stage1407@users.noreply.github.com>
2022-06-09 18:55:53 +0200

    Update annotator.py
    Refactored, restructured, commented and documented

--- src/scripts/btma/annotator.py
+++ src/scripts/btma/annotator.py
@@ -8,57 +8,105 @@ to which module this class belongs
 @author: alexander stage    
 """
 
-# Imports
-
+#* Imports
+import io
 import pathlib as path
 import csv
 import subprocess as sp
 import os
 import shutil as sh
 import sys, time, threading
-#import panda as pn
-#import numpy as np
 
-# Load 'static' values
+#* Define used methods
 
-ontology_iri = 'http://openenergy-platform.org/ontology/oeo'
+def find_root_dir(dirname : str) -> path.Path:
+    """
+    Finds the parent directory named `dirname` of the current directory.
 
-modules = ['oeo-shared',
-           'oeo-physical',
-           'oeo-social',
-           'oeo-model']
+    Args:
+        `dirname (str)`: name of the current file directory
 
-def find_root_dir(dirname : str):
-    return find_root_dir_(path.Path('.').absolute(), dirname)
+    Returns:
+        `path.Path`: parent path of the directory with the name `dirname` in this project
+    """
+    return __find_root_dir(path.Path('.').absolute(), dirname)
 
-def find_root_dir_(cur_path : path.Path, dirname : str):
+def __find_root_dir(cur_path : path.Path, dirname : str) -> path.Path:
     if cur_path.name == dirname:
         return cur_path
     else:
-        return find_root_dir_(cur_path.parent, dirname)
+        return __find_root_dir(cur_path.parent, dirname)
+
+#* Create overview about IRIs and there belonging
+
+def extract_belongings(ids : set, ids_per_module) -> list[list]:
+    """
+    Extracts the belonging of each ID
+
+    Args:
+        `ids (set)`: set of IDs
+        `ids_per_module (_type_)`: partition of `ids` in modules
+
+    Returns:
+        `list[list]`: list of IDs per modules
+    """
+    belongings : list[list] = []
+
+    for (i,id) in enumerate(ids):
+        belongings.append([])
+        for (j,module_ids) in enumerate(ids_per_module):
+            if id in module_ids:
+                belongings[i].append(modules[j])
+                
+    return belongings
+
+def extract_prefixes(onto : io.TextIOWrapper) -> list:
+    """
+    Extracts Prefixes of the ontology `onto`.
+
+    Args:
+        `onto (io.TextIOWrapper)`: Ontology in OMN format
+
+    Returns:
+        `list`: the prefixes used in `onto`
+    """
+    prefixes = []
+    lines = onto.readlines()
     
-src_path = find_root_dir('src')
+    for line in lines:
+        if line.startswith('Prefix:'):
+            prefixes.append(line[8:-1])             #* <- Abhängig vom Prefix
+        else:
+            break
+        
+    return prefixes
+    
+def extract_types(ids) -> list:
+    """
+    Assigns each ID to its type.
 
-changes_exists = False
-running = True
+    Args:
+        `ids`: list of IDs
 
-# src_path = path.Path('src').absolute()            <- use in IDE
-script_path = src_path / 'scripts'
+    Returns:
+        `list`: list of the types of each ID
+    """
+    types = []
+    for i, elem in enumerate(ids):
+            types.append(elem[1])
+            ids[i] = elem[0]
+    return types
+    
+def extract_IDs_Types(path : path.Path) -> list:
+    """
+    Extract ID and Type of each entity.
 
-omn_path = src_path / 'ontology' / 'edits'
-btma_path = script_path / 'btma'
-csv_path = btma_path / 'data'
-db_path = btma_path / 'database'
-exp_path = btma_path / 'exports'
-onto_path = src_path / 'ontology'
-owl_path = onto_path / 'imports'
-robot_path = btma_path / 'robot.jar'
-pre_path = btma_path / 'prefixes'
+    Args:
+        `path (path.Path)`: The path of a CSV-file with IDs and Types in the first and second column
 
-def export_prefixes():
-    pass
-
-def extract_IDs_Types(path) -> list:
+    Returns:
+        `list`: List of Tuples e.g. [(<ID_1>,<Type_1>),...,(<ID_N>,<Type_N>)]
+    """
     ids = []
     with open(path) as file:
         CSVreader = csv.reader(file)
@@ -67,9 +115,55 @@ def extract_IDs_Types(path) -> list:
                 ids.append((row[0], row[1]))
     return ids
 
+def serialize_prefixes(prefixes : list) -> str:
+    """
+    Serialize the prefixes for `'--add-prefix'` in the `robot` command
+    
+    Args:
+        `prefixes (list)`: list of IRIs
+
+    Returns:
+        ``str`: Concatenated '--add-prefix IRI' for each IRI
+    """
+    prefix_str = ""
+    
+    for prefix in prefixes:
+            prefix_ = ""
+            for c in prefix:
+                if c != '<' and c != '>':
+                    prefix_ += c
+
+            prefix_str += '--add-prefix \"{p}\" '.format(p=prefix_)
+
+    return prefix_str
+
+def reset_btma():
+    """
+    Reset the project at the end of the script.
+    """
+    os.remove(script_path / 'btma' / "template.csv")
+    try:
+        sh.rmtree(db_path)
+    except:
+        pass
+    os.rename(csv_path, db_path)
+    sh.rmtree(exp_path)
+    
+def __loadingAnimation(process):
+    while running:
+        chars = '/—\|' 
+        for char in chars:
+            sys.stdout.write('\r'+'Running annotator... '+char)
+            time.sleep(.1)
+            sys.stdout.flush()
+    if not changed:
+        sys.stdout.write('\r'+'Annotations are already up to date.\n')
+    sys.stdout.write('\r'+'Finished!                    ')
+
 def main():
     global running
-    global changes_exists
+    global changed
+    
     try:
         os.mkdir(csv_path)
     except:
@@ -80,19 +174,10 @@ def main():
     ids_per_module = [_,_,_,_]
 
     prefixes = []
-    
     oeo = open(src_path / 'ontology' / 'oeo.omn')
+    prefixes = extract_prefixes(oeo)
 
-    lines = oeo.readlines()
-    
-    for line in lines:
-        if line.startswith('Prefix:'):
-            prefixes.append(line[8:-1])             #* Abhängig vom Prefix
-        else:
-            break
-        
-
-    # Load necessary IRIs of the different ontology-modules
+    #* Load necessary IRIs of the different ontology-modules
 
     ids : set = set([])
     for (i,module) in enumerate(modules):
@@ -100,10 +185,11 @@ def main():
         exp = csv_path / "{m}.csv".format(m=module)
         
         sp.call('java -jar {jar} export --input {m} \
-                                        --prefix "OEO: http://openenergy-platform.org/ontology/oeo/OEO_" \
+                                        --prefix "OEO: {iri}" \
                                         --header "{c}" \
                                         --export {e}'.format(jar=robot_path,
                                                              m=mod, 
+                                                             iri=ontology_iri + '/OEO_', 
                                                              c=col, 
                                                              e=exp), shell=True)
         
@@ -113,7 +199,7 @@ def main():
 
     ids = list(ids)
 
-    # Compare new data with database
+    #* Compare new data with database
 
     old_ids_per_module = [_,_,_,_]
     try:
@@ -122,101 +208,61 @@ def main():
     except:
         pass
     
-    changes_exists = old_ids_per_module != ids_per_module
-    if changes_exists:
-
-        # Create overview about IRIs and there belonging
+    changed = old_ids_per_module != ids_per_module
+    #* edit omn if and only if omn was changed
+    if changed:
+        #* Create the CSV template with the help of an 'overview'                            
         
-        """
-        #! Check if duplicates exists in ids
-        for i, x in enumerate(ids):
-            for j, y in enumerate(ids):
-                if i != j and x == y:
-                    print("1:" + str(x))
+        belongings : list[list] = extract_belongings(ids, ids_per_module)
         
-        #! Check if duplicates exists in the seperate lists in ids_per_module
-        for li in ids_per_module:
-            for i, x in enumerate(li):
-                for j, y in enumerate(li):
-                    if i != j and x == y:
-                        print("2:" + str(x))                                           
+        types = extract_types(ids)
         
-        """
-        
-        belonging = []
-
-        for (i,id) in enumerate(ids):
-            belonging.append([])
-            for (j,module_ids) in enumerate(ids_per_module):
-                if id in module_ids:
-                    belonging[i].append(modules[j])
-
-        # Create the CSV template with the help of the overview
-        
-        types = []
-        
-        for i, elem in enumerate(ids):
-            types.append(elem[1])
-            ids[i] = elem[0]
-        
-        belongs_to_module = list(zip(ids, types, belonging))
+        belongs_to_module = list(zip(ids, types, belongings))
         
         csv_table = [["ID", "Entity Type", "OEO:00260001"],
                      ["ID", "TYPE", "AI OEO:00260001"]]
-
+        
         csv_table_per_module = {'oeo-shared' : csv_table, 
                                 'oeo-physical' : [a for a in csv_table], 
                                 'oeo-social' : [a for a in csv_table], 
                                 'oeo-model' : [a for a in csv_table]}
 
-        # Add only one belonging to an entity
-        # regarding to following rule:
-        # IF in shared THEN annotate as shared ELSE annotate as the first in the list
-        # Using Dictionaries instead of lists <dict>[<string>] instead of <list>[<int>] for better maintenance 
-        #?DOMAIN EXPERTS: is another rule needed then priority
+        #* Adds only one belonging to an entity
+        #* regarding to following rule:
+        #* First: Shared; Second: Physical; Third: Model; Fourth: Social
+        #* Using Dictionaries instead of lists <dict>[<string>] instead of <list>[<int>] for better maintenance 
         for ids, type_, belonging in belongs_to_module:
             if 'oeo-shared' in belonging:
                 csv_table_per_module['oeo-shared'].append([ids, 'owl:' + type_, ontology_iri + '/oeo-shared'])
             elif 'oeo-physical' in belonging:
                 csv_table_per_module['oeo-physical'].append([ids, 'owl:' + type_, ontology_iri + '/oeo-physical'])
-            elif 'oeo-social' in belonging:
-                csv_table_per_module['oeo-social'].append([ids, 'owl:' + type_, ontology_iri + '/oeo-social'])
             elif 'oeo-model' in belonging:
                 csv_table_per_module['oeo-model'].append([ids, 'owl:' + type_, ontology_iri + '/oeo-model'])
-                
-        prefix_adder = ""
-
-        for prefix in prefixes[1:]:
-            prefix_ = ""
-            for c in prefix:
-                if c != '<' and c != '>':
-                    prefix_ += c
-            prefix_adder += '--add-prefix \"{p}\" '.format(p=prefix_)
+            elif 'oeo-social' in belonging:
+                csv_table_per_module['oeo-social'].append([ids, 'owl:' + type_, ontology_iri + '/oeo-social'])
+                        
+        add_prefixes = serialize_prefixes(prefixes[1:])
                 
         for module in csv_table_per_module:
-            f = open(script_path / 'btma' / "template.csv", 'w', newline='')
+            
+            with open(script_path / 'btma' / 'template.csv', 'w', newline='') as f:
+                writer = csv.writer(f)
+                writer.writerows(csv_table_per_module[module])
 
-            writer = csv.writer(f)
-
-            writer.writerows(csv_table_per_module[module])
-
-            f.close()
-
-            # Create OMN with help of the CSV template
+            #* Create OMN with help of the CSV template
             
             sp.call('java -jar {jar} template --template {template} \
-                                              --add-prefix \"OEO: {iri}OEO_\" \
+                                              --add-prefix \"OEO: {iri}\" \
                                               --add-prefix \"owl: http://www.w3.org/2002/07/owl#\" \
                                               --output {out}'.format(jar=robot_path,
                                                                      template=script_path / 'btma' / 'template.csv',
-                                                                     iri=ontology_iri + '/',
-                                                                     pre=prefix_adder[-1],
+                                                                     iri=ontology_iri + '/OEO_',
                                                                      out=exp_path / 'belongs-to-{m}-annotation.omn'.format(m=module)), shell=True)
         
-        # MERGE OMN files with the oeo modules
+        #* MERGE OMN files with the oeo modules
         
         for module in csv_table_per_module:
-            # Valid Call
+            #TODO: Valid Call
             """
             sp.call('java -jar {jar} merge --input {out} \
                                            --input {inp} \
@@ -229,51 +275,54 @@ def main():
                                                                               pre=prefix_adder[:-1],
                                                                               iri=ontology_iri + '/'), shell=True)
             """
-            # Test Call
+            #TODO: Test Call
             sp.call('java -jar {jar} merge --input {out} \
                                            --input {inp} \
-                                           --add-prefix \"OEO: {iri}OEO_\" \
+                                           --add-prefix \"OEO: {iri}\" \
                                            {pre} \
                                            --output {out_} \
                                            --include-annotations true'.format(jar=robot_path,
                                                                               out=omn_path / (module + '.omn'),
                                                                               out_=omn_path / (module + '-new.omn'),
                                                                               inp=exp_path / 'belongs-to-{m}-annotation.omn'.format(m=module),
-                                                                              pre=prefix_adder[:-1],
-                                                                              iri=ontology_iri + '/'), shell=True)
-            
-        os.remove(script_path / 'btma' / "template.csv")
-    
-        try:
-            sh.rmtree(db_path)
-        except:
-            pass
-
-        os.rename(csv_path, db_path)
+                                                                              pre=add_prefixes[:-1],
+                                                                              iri=ontology_iri + '/OEO_'), shell=True)
+                
+        reset_btma()
         
-        sh.rmtree(exp_path)
-
     else:
         sh.rmtree(csv_path)
         
     running = False    
 
-def loadingAnimation(process):
-    #global running
-    while running:
-        chars = '/—\|' 
-        for char in chars:
-            sys.stdout.write('\r'+'Running annotator... '+char)
-            time.sleep(.1)
-            sys.stdout.flush()
-    #sys.stdout.flush()
-    if not changes_exists:
-        sys.stdout.write('\r'+'Annotations are already up to date.\n')
-    sys.stdout.write('\r'+'Finished!                    ')
+#* Script start here
+#* Load 'static' values
+
+ontology_iri = 'http://openenergy-platform.org/ontology/oeo'
+
+changed = False
+running = True
+modules = ['oeo-shared',
+           'oeo-physical',
+           'oeo-social',
+           'oeo-model']
+
+#* src_path = path.Path('src').absolute()            #<- For development use this in your IDE instead of 'find_root_dir'
+src_path = find_root_dir('src')
+script_path = src_path / 'scripts'
+omn_path = src_path / 'ontology' / 'edits'
+btma_path = script_path / 'btma'
+csv_path = btma_path / 'data'
+db_path = btma_path / 'database'
+exp_path = btma_path / 'exports'
+onto_path = src_path / 'ontology'
+owl_path = onto_path / 'imports'
+robot_path = btma_path / 'robot.jar'
+pre_path = btma_path / 'prefixes'
             
 if __name__ == '__main__':
     loading_process = threading.Thread(target=main)
     loading_process.start()
 
-    loadingAnimation(loading_process)
+    __loadingAnimation(loading_process)
     loading_process.join()
