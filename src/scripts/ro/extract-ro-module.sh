84e9eb8ba68be79bb40f766d2ca9ed91710e63e0
markus-rothkoetter <markus.rothkoetter@ovgu.de>
2022-08-12 16:24:39 +0200

    Add removal script for oboInOwl:inSubset #1154

--- src/scripts/ro/extract-ro-module.sh
+++ src/scripts/ro/extract-ro-module.sh
@@ -6,6 +6,8 @@ robot merge --input ro-full-download.owl extract --method MIREOT --lower-terms r
 
 robot merge --input ro-extracted-w-hierarchy.owl --input ro-extracted-n-hierarchy.owl annotate --ontology-iri http://openenergy-platform.org/ontology/oeo/imports/ro-module.owl --version-iri http://openenergy-platform.org/ontology/oeo/dev/imports/ro-module.owl --annotation rdfs:comment "This file contains externally imported content from the Relations Ontology (RO) for import into the Open Energy Ontology (OEO). It is automatically extracted using ROBOT from the term list ro-terms.txt." --output ../../ontology/imports/ro-extracted.owl
 
+robot remove --input ro-extracted.owl --term oboInOwl:inSubset --output ro-extracted.owl
+
 
 rm ro-full-download.owl
 rm ro-extracted-w-hierarchy.owl
