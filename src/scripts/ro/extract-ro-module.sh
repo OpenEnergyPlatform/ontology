688e273368b0e03ff2dd27f83556027920726c56
markus-rothkoetter <markus.rothkoetter@ovgu.de>
2022-08-12 13:06:31 +0200

    Fix wrong IRIs of ontology #1154

--- src/scripts/ro/extract-ro-module.sh
+++ src/scripts/ro/extract-ro-module.sh
@@ -4,10 +4,9 @@ robot merge --input ro-full-download.owl extract --method MIREOT --lower-terms r
 
 robot merge --input ro-full-download.owl extract --method MIREOT --lower-terms ro-extracted-n-hierarchy.txt --upper-term owl:topObjectProperty --intermediates none --output ro-extracted-n-hierarchy.owl
 
-robot merge --input ro-extracted-w-hierarchy.owl --input ro-extracted-n-hierarchy.owl annotate --ontology-iri http://open-energy-ontology.org/ontology/imports/ro-module.owl --version-iri http://open-energy-ontology.org/ontology/imports/ro-module.owl --annotation rdfs:comment "This file contains externally imported content from the Relations Ontology (RO) for import into the Open Energy Ontology (OEO). It is automatically extracted using ROBOT from the term list ro-terms.txt." --output ../../ontology/imports/ro-extracted.owl
+robot merge --input ro-extracted-w-hierarchy.owl --input ro-extracted-n-hierarchy.owl annotate --ontology-iri http://openenergy-platform.org/ontology/oeo/imports/ro-module.owl --version-iri http://openenergy-platform.org/ontology/oeo/dev/imports/ro-module.owl --annotation rdfs:comment "This file contains externally imported content from the Relations Ontology (RO) for import into the Open Energy Ontology (OEO). It is automatically extracted using ROBOT from the term list ro-terms.txt." --output ../../ontology/imports/ro-extracted.owl
 
 
 rm ro-full-download.owl
 rm ro-extracted-w-hierarchy.owl
 rm ro-extracted-n-hierarchy.owl
-
