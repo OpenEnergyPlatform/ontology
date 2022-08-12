64c3b7874f31980f0eca789d5ea18e206fd528d7
markus-rothkoetter <markus.rothkoetter@ovgu.de>
2022-08-12 12:42:19 +0200

    Rename target module to `ro-extracted.owl` #1154

--- src/scripts/ro/extract-ro-module.sh
+++ src/scripts/ro/extract-ro-module.sh
@@ -4,7 +4,7 @@ robot merge --input ro-full-download.owl extract --method MIREOT --lower-terms r
 
 robot merge --input ro-full-download.owl extract --method MIREOT --lower-terms ro-extracted-n-hierarchy.txt --upper-term owl:topObjectProperty --intermediates none --output ro-extracted-n-hierarchy.owl
 
-robot merge --input ro-extracted-w-hierarchy.owl --input ro-extracted-n-hierarchy.owl annotate --ontology-iri http://open-energy-ontology.org/ontology/imports/ro-module.owl --version-iri http://open-energy-ontology.org/ontology/imports/ro-module.owl --annotation rdfs:comment "This file contains externally imported content from the Relations Ontology (RO) for import into the Open Energy Ontology (OEO). It is automatically extracted using ROBOT from the term list ro-terms.txt." --output ../../ontology/imports/ro-module.owl
+robot merge --input ro-extracted-w-hierarchy.owl --input ro-extracted-n-hierarchy.owl annotate --ontology-iri http://open-energy-ontology.org/ontology/imports/ro-module.owl --version-iri http://open-energy-ontology.org/ontology/imports/ro-module.owl --annotation rdfs:comment "This file contains externally imported content from the Relations Ontology (RO) for import into the Open Energy Ontology (OEO). It is automatically extracted using ROBOT from the term list ro-terms.txt." --output ../../ontology/imports/ro-extracted.owl
 
 
 rm ro-full-download.owl
