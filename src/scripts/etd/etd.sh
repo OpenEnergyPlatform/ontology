4f8fd48565a0ab825dc1905428cb5f7f13c34505
stage1407 <86624154+stage1407@users.noreply.github.com>
2022-09-02 16:05:53 +0200

    Works for the most cases.

--- src/scripts/etd/etd.sh
+++ src/scripts/etd/etd.sh
@@ -1,50 +1,23 @@
 #!/bin/bash
-
-########################################
-#   ETD SCRIPT                         #
-########################################
-
-# Getting path variables
-
-ETD_PATH="$PWD"
-ONTO_PATH="$(dirname "$SCRIPT_PATH")""ontology"
-
-#TODO: TEST
-
-echo "$SCRIPT_PATH"
-echo "$ONTO_PATH"
-
-# Create exported CSV from ontology
-
-robot merge --input "$ONTO_PATH""oeo.omn" \
-            --input "$ONTO_PATH""edits/oeo-model.omn" \
+SCRIPT_PATH="$PWD"
+ONTO_PATH="$(dirname "$(dirname "$SCRIPT_PATH")")""/ontology/"
+robot merge --input "$ONTO_PATH""edits/oeo-model.omn" \
             --input "$ONTO_PATH""edits/oeo-physical.omn" \
             --input "$ONTO_PATH""edits/oeo-shared.omn" \
             --input "$ONTO_PATH""edits/oeo-social.omn" \
             --include-annotations true \
-    export  --header "ID|LABEL|iao:definition" \
+    export  --header "ID|LABEL|definition" \
+            --prefix "OEO: http://openenergy-platform.org/ontology/oeo/OEO_" \
+            --sort "LABEL" \
             --export "$ETD_PATH""etd.csv"
-
-#TODO: TEST
-
-# Create directory with markdown
-
-STRING="|ID|Name|Definition|\n|:--|:---:|----:|"
-
-if ! [ -e ETD.md ];
-then
-    cat > etd.md
-fi
-
-
-
-
-while IFS="," read -r rec1 rec2 rec3;
+LINE1="|ID|Name|Definition|"
+LINE2="|:-|:---|:-------|"
+echo "half terminated"
+echo "nearly terminated"
+echo $LINE1 >> ETD.md
+echo $LINE2 >> ETD.md
+while IFS=, read -r rec1 rec2 rec3
 do
-    STRING="\n$STRING|$rec1|$rec2|$rec3|"
-    echo "$STRING" >> ETD.md
-done < etd.csv
-
-#TODO: TEST
-
-echo "$STRING"
+    echo "|$rec1|$rec2|$rec3|" >> ETD.md
+done < <(tail -n +2 etd.csv)
+rm etd.csv
