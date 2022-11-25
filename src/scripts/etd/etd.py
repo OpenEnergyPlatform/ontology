a20dd8c66cca90d3e5382c9716c5ad534d5bf7af
Eugenio Salvador Arellano Ruiz <eugenio.arellanoruiz@dlr.de>
2022-11-25 16:16:03 +0100

    Added some flexibility to etd

--- src/scripts/etd/etd.py
+++ src/scripts/etd/etd.py
@@ -5,6 +5,7 @@ import re
 import string
 import pathlib
 warnings.filterwarnings("ignore")
+import sys
 
 GLOSSARY_HEADER = """# Existing Terms and Definitions
 
@@ -13,8 +14,13 @@ BASE_LINK_WIKI = "https://github.com/OpenEnergyPlatform/ontology/wiki/"
 BASE_IRI = "http://openenergy-platform.org/ontology/oeo/"
 
 if __name__ == '__main__':
+    arguments = sys.argv[1:]
+    if len(arguments) > 0:
+        target_path = arguments[0]
+    else:
+        target_path = 'src/scripts/etd'
     cwd = os.getcwd()
-    df = pd.read_excel('src/scripts/etd/etd.xlsx')
+    df = pd.read_excel(pathlib.Path(target_path).joinpath("etd.xlsx").as_posix())
 
     df = df.replace('\n', '<br>', regex=True)
     df = df.sort_values("LABEL", key=lambda col: col.str.strip().str.lower())
@@ -41,7 +47,7 @@ if __name__ == '__main__':
         table = title + buffer + "\n\n"
         #df.to_markdown(buf=cwd + "/src/scripts/etd/ETD.md", index=False)
         output += table
-    with open(cwd + f"/src/scripts/etd/glossary/glossary.md", "w") as fil:
+    with open(pathlib.Path(target_path).joinpath("glossary.md").as_posix(), "w") as fil:
         fil.write(output)
 
     # create csv output
@@ -49,4 +55,4 @@ if __name__ == '__main__':
     df_csv["ID"] = df_csv["ID"].str.replace("http://openenergy-platform.org/ontology/oeo/oeo-physical/", "")
     df_csv["ID"] = df_csv["ID"].str.replace("http://openenergy-platform.org/ontology/oeo/oeo-model/", "")
     df_csv["ID"] = df_csv["ID"].str.replace(":", "_")
-    df_csv.to_csv(cwd + f"/src/scripts/etd/glossary/glossary.csv", index=False)
+    df_csv.to_csv(pathlib.Path(target_path).joinpath("glossary.csv").as_posix(), index=False)
