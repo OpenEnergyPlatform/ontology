ad5359c0f513df233a7bb2007103227f8a97c9b2
stage1407 <86624154+stage1407@users.noreply.github.com>
2023-02-06 18:54:07 +0100

    Added annotation command and adjusted structure of the script

--- src/scripts/iao/extract-iao-module.bat
+++ src/scripts/iao/extract-iao-module.bat
@@ -1,1 +1,6 @@
-robot merge --input iao.owl extract --method MIREOT --branch-from-term "http://purl.obolibrary.org/obo/IAO_0000030" annotate --ontology-iri http://open-energy-ontology.org/ontology/imports/iao-module.owl --version-iri http://open-energy-ontology.org/ontology/imports/iao-module.owl --annotation rdfs:comment "This file contains externally imported content from the Information Artifact Ontology (IAO) for import into the Open Energy Ontology (OEO). It is automatically extracted using ROBOT." --output ../../ontology/imports/iao-module.owl
+Rem Merging the iao.owl with the extracted modules
+robot merge --input iao.owl extract --method MIREOT --branch-from-term "http://purl.obolibrary.org/obo/IAO_0000030" --output ../../ontology/imports/iao-module.owl
+Rem Annotates the output with a commentary to the origin of the content 
+robot annotate --input ../../ontology/imports/iao-module.owl --ontology-iri http://open-energy-ontology.org/ontology/imports/iao-module.owl --version-iri http://open-energy-ontology.org/ontology/imports/iao-module.owl --annotation rdfs:comment "This file contains externally imported content from the Information Artifact Ontology (IAO) for import into the Open Energy Ontology (OEO). It is automatically extracted using ROBOT." --output ../../ontology/imports/iao-module.owl
+Rem Annotates each axiom with the ontology IRI, using prov:wasDerivedFrom
+robot annotate --input ../../ontology/imports/iao-module.owl --annotate-derived-from true --annotate-defined-by true --output ../../ontology/imports/iao-module.owl
